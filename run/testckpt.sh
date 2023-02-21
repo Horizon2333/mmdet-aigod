215957225e503fd0a4e921ae5f4327a5edbf6877
hanyue1648 <1015985094@qq.com>
2023-02-21 11:11:58 +0000

    add more sh to run and better log in test

--- run/testckpt.sh
+++ run/testckpt.sh
@@ -4,7 +4,7 @@ export CUDA_VISIBLE_DEVICES=5
 # python tools/test.py configs/rfla/aitod_fcos_r50_rfla_kld_1x.py pretrained/aitod_fcos_r50_rfla_kld_1x.pth --eval bbox
 
 # evaluate Faster R-CNN
-# python tools/test.py configs/rfla/aitod_faster_r50_rfla_kld_1x.py pretrained/aitod_faster_r50_rfla_kld_1x.pth --eval bbox
+python tools/test.py configs/rfla/aitod_faster_r50_rfla_kld_1x.py pretrained/aitod_faster_r50_rfla_kld_1x.pth --eval bbox
 
 # evaluate Cascade R-CNN
-python tools/test.py configs/rfla/aitod_cascade_r50_rfla_kld_1x.py pretrained/aitod_cascade_r50_rfla_kld_1x.pth --eval bbox
+# python tools/test.py configs/rfla/aitod_cascade_r50_rfla_kld_1x.py pretrained/aitod_cascade_r50_rfla_kld_1x.pth --eval bbox
---
+++ run/testfilter.sh
@@ -0,0 +1,3 @@
+export CUDA_VISIBLE_DEVICES=5
+
+python tools/test.py configs/rfla/filter_faster_r50_rfla_kld_1x.py work_dirs/filter_faster_r50_rfla_kld_1x/epoch_12.pth --eval bbox
